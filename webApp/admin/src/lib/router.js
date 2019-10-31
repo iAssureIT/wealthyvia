f4ea101fb6d797e4f0dc2d4430fcd677ed2145f4
iAssureIT <priyankalewade96@gmail.com>
2019-10-31 15:27:59 +0530

    Priyanka L added changes

--- webApp/admin/src/lib/router.js
+++ webApp/admin/src/lib/router.js
@@ -9,9 +9,6 @@ import EditUserProfile from '../coreAdmin/userManagement/UM/EditUserProfile.js';
 import UMRolesList from '../coreAdmin/userManagement/Roles/UMRolesList.js';
 
 import CompanySetting from '../coreAdmin/companysetting/Components/CompanySetting.js';
-import addWorkspace   from '../cofficAdmin/WorkspaceManagement/addWorkspace.js';
-import addSubscription   from '../cofficAdmin/Subscription/addSubscription.js';
-import editSubscription   from '../cofficAdmin/Subscription/editSubscription.js';
 // import ViewTemplates from '../coreAdmin/NotificationManagement/ViewTemplates.js';
 // import Header from '/Users/omkar/Documents/Reactjs/TGK/src/coreAdmin/dashboard/Dashboard.js';
 
@@ -20,7 +17,6 @@ import editSubscription   from '../cofficAdmin/Subscription/editSubscription.js'
 
 export const routes  =(
     <div className="col-lg-10 col-lg-offset-2">
-    {console.log("check")}
         <Router>
     }
 {/*            <Route path="/umlistofusers" component={UMListOfUsers} exact />*/}
