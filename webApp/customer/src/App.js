bf0c08599a01bf8c92ce7a5b93a0b45967daeff2
iAssureIT <priyankalewade96@gmail.com>
2019-11-25 18:21:25 +0530

    PL added changes

--- webApp/customer/src/App.js
+++ webApp/customer/src/App.js
@@ -6,8 +6,8 @@ import axios                    from "axios";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//axios.defaults.baseURL = 'https://api.wealthyvia.com';
-axios.defaults.baseURL = 'http://wealthyviapi.iassureit.com';
+axios.defaults.baseURL = 'https://api.wealthyvia.com';
+//axios.defaults.baseURL = 'http://wealthyviapi.iassureit.com';
 //axios.defaults.baseURL = 'http://localhost:3066';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
