ac001b74b2d0ae80cb7229f86dd9573c64776b33
Root <monikapawashe25@gmail.com>
2020-07-27 17:53:58 +0530

    monika added chart on product page

--- webApp/website/src/pages/ProductPage/ProductPage.js
+++ webApp/website/src/pages/ProductPage/ProductPage.js
@@ -378,6 +378,7 @@ export default class ProductPage extends Component {
                 
                 </div>
               </div>
+              <ProductChart offeringName="Safe Heavan" />
               <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 backColorGray onHoverEffect" >
                 <div className="row">
                
@@ -651,8 +652,7 @@ export default class ProductPage extends Component {
                 </div>
            </div>
 
-            <ProductChart offeringName="Safeheaven" />
-
+            
               <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 backColorGray onHoverEffect" >
                 <div className="row">
                   <div className="col-lg-6 col-md-6 hidden-sm hidden-xs imageContainer ">
