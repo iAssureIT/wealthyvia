7df11a1aad8d7d6598d4016fef90ad362a548444
Root <monikapawashe25@gmail.com>
2020-07-29 09:53:14 +0530

    monika added filewise product rate chart

--- API/nodemon.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dbname: "weathyvia",
-    port: 3015,
-    JWT_KEY: 'secret',
-    accessKeyId: 'AKIARNLDQCAGULPCSOND',
-    secretAccessKey: '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',
-    bucket: 'testwealthyvia',
-    region: 'ap-south-1'
-} 
